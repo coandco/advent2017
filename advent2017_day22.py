583cd934efa89b6c0b5ae0d27be8fcd95708bcac
Clint Olson <coandco@gmail.com>
2017-12-21 23:22:21 -0800

    Cleaned up day 22.

--- advent2017_day22.py
+++ advent2017_day22.py
@@ -1,3 +1,5 @@
+import collections
+
 INPUT = """.......##.#..####.#....##
 ..###....###.####..##.##.
 #..####.#....#.#....##...
@@ -25,97 +27,57 @@ INPUT = """.......##.#..####.#....##
 .....##.#....##...#.....#""".split("\n")
 
 
-def location_status(set_dict, location):
-    for status in set_dict:
-        if tuple(location) in set_dict[status]:
-            return status
-    return '.'
-
-
-def set_status(set_dict, location, new_status):
-    for status in set_dict:
-        if status == new_status:
-            set_dict[status].add(tuple(location))
-        else:
-            set_dict[status].discard(tuple(location))
-    return set_dict
-
-
-def process_burst_v1(set_dict, direction, location):
+def process_burst(set_dict, direction, location, transform):
     new_direction = list(direction)
-    cur_loc_status = location_status(set_dict, location)
-    infected_this_burst = False
+    cur_loc_status = set_dict[tuple(location)]
+    set_dict[tuple(location)] = transform[cur_loc_status]
     if cur_loc_status == '.':
-        set_dict = set_status(set_dict, location, '#')
-        infected_this_burst = True
-        # Turn left
-        new_direction[0], new_direction[1] = -new_direction[1], new_direction[0]
-    elif cur_loc_status == '#':
-        set_dict = set_status(set_dict, location, '.')
-        # Turn right
-        new_direction[0], new_direction[1] = new_direction[1], -new_direction[0]
-    else:
-        raise Exception("Unknown location status: %s" % cur_loc_status)
-
-    new_location = [sum(x) for x in zip(new_direction, location)]
-    return set_dict, new_direction, new_location, infected_this_burst
-
-
-def process_burst_v2(set_dict, direction, location):
-    new_direction = list(direction)
-    cur_loc_status = location_status(set_dict, location)
-    infected_this_burst = False
-    if cur_loc_status == '.':
-        set_dict = set_status(set_dict, location, 'W')
         # Turn left
         new_direction[0], new_direction[1] = -new_direction[1], new_direction[0]
     elif cur_loc_status == 'W':
-        set_dict = set_status(set_dict, location, '#')
-        infected_this_burst = True
         # No direction change
+        pass
     elif cur_loc_status == '#':
-        set_dict = set_status(set_dict, location, 'F')
         # Turn right
         new_direction[0], new_direction[1] = new_direction[1], -new_direction[0]
     elif cur_loc_status == 'F':
-        set_dict = set_status(set_dict, location, '.')
         # Reverse direction
         new_direction[0], new_direction[1] = -new_direction[0], -new_direction[1]
     else:
         raise Exception("Unknown location status: %s" % cur_loc_status)
 
+    infected_this_burst = (transform[cur_loc_status] == '#')
     new_location = [sum(x) for x in zip(new_direction, location)]
-    return set_dict, new_direction, new_location, infected_this_burst
+    return new_direction, new_location, infected_this_burst
 
-INFECTED_NODES = set()
+
+starting_dict = collections.defaultdict(lambda: '.')
 for i, line in enumerate(INPUT):
     for j, char in enumerate(line):
         if char == "#":
-            INFECTED_NODES.add((i-12, j-12))
+            starting_dict[tuple([i - (len(line) // 2), j - (len(line) // 2)])] = '#'
 
-set_dict = {'#': set(INFECTED_NODES), 'W': set(), 'F': set()}
-current_direction = [-1, 0]
-current_location = [0, 0]
+set_dict = starting_dict.copy()
+cur_dir = [-1, 0]
+cur_loc = [0, 0]
 infection_count = 0
+V1_TRANSFORM = {'.': '#', '#': '.'}
 
 for i in xrange(10000):
-    infected_set, current_direction, current_location, new_infection = process_burst_v1(set_dict,
-                                                                                        current_direction,
-                                                                                        current_location)
+    cur_dir, cur_loc, new_infection = process_burst(set_dict, cur_dir, cur_loc, V1_TRANSFORM)
     if new_infection:
         infection_count += 1
 
 print("Final infection count for v1: %d" % infection_count)
 
-set_dict = {'#': set(INFECTED_NODES), 'W': set(), 'F': set()}
-current_direction = [-1, 0]
-current_location = [0, 0]
+set_dict = starting_dict.copy()
+cur_dir = [-1, 0]
+cur_loc = [0, 0]
 infection_count = 0
+V2_TRANSFORM = {'.': 'W', 'W': '#', '#': 'F', 'F': '.'}
 
 for i in xrange(10000000):
-    infected_set, current_direction, current_location, new_infection = process_burst_v2(set_dict,
-                                                                                        current_direction,
-                                                                                        current_location)
+    cur_dir, cur_loc, new_infection = process_burst(set_dict, cur_dir, cur_loc, V2_TRANSFORM)
     if new_infection:
         infection_count += 1
     if (i + 1) % 1000000 == 0:
