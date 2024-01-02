104ddfa9cda813cfed5691b53b707f74556f8c78
Clint Olson <coandco@gmail.com>
2024-01-02 00:08:35 -0800

    Update day 15 with modern sensibilities

--- advent2017_day15.py
+++ advent2017_day15.py
@@ -1,40 +1,23 @@
-INPUT_A = 277
-INPUT_B = 349
+from utils import read_data
+import time
 
 
-def gen_value(start, factor, multiple=1):
-    current = (start * factor) % 2147483647
-    while current % multiple != 0:
-        current = (current * factor) % 2147483647
+def gen_value(next: int, factor: int, multiple=1):
     while True:
-        yield current
-        current = (current * factor) % 2147483647
-        while current % multiple != 0:
-            current = (current * factor) % 2147483647
+        next = (next * factor) % 2147483647
+        if next % multiple == 0:
+            yield next & 0xffff
 
 
-gen_a = gen_value(INPUT_A, 16807)
-gen_b = gen_value(INPUT_B, 48271)
-total_matches = 0
-for i in xrange(40000000):
-    string_a = format(next(gen_a), "#032b")[-16:]
-    string_b = format(next(gen_b), "#032b")[-16:]
-    if string_a == string_b:
-        total_matches += 1
-    if i % 1000000 == 0:
-        print("V1 iterated %d times, %d matches so far" % (i, total_matches))
+def main():
+    starts = [int(x.split()[-1]) for x in read_data().splitlines()]
+    gen_a, gen_b = gen_value(starts[0], 16807), gen_value(starts[1], 48271)
+    print(f"Part one: {sum(next(gen_a) == next(gen_b) for _ in range(40_000_000))}")
+    gen_a, gen_b = gen_value(starts[0], 16807, 4), gen_value(starts[1], 48271, 8)
+    print(f"Part two: {sum(next(gen_a) == next(gen_b) for _ in range(5_000_000))}")
+    
 
-print("Total matches for v1: %d" % total_matches)
-
-gen_a = gen_value(INPUT_A, 16807, 4)
-gen_b = gen_value(INPUT_B, 48271, 8)
-total_matches = 0
-for i in xrange(5000000):
-    string_a = format(next(gen_a), "#032b")[-16:]
-    string_b = format(next(gen_b), "#032b")[-16:]
-    if string_a == string_b:
-        total_matches += 1
-    if i % 1000000 == 0:
-        print("V2 iterated %d times, %d matches so far" % (i, total_matches))
-
-print("Total matches for v2: %d" % total_matches)
+if __name__ == '__main__':
+    start = time.monotonic()
+    main()
+    print(f"Time: {time.monotonic()-start}")
