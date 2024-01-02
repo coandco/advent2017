f55d0913d70004edbcb593bd47c20a656903314f
Clint Olson <coandco@gmail.com>
2024-01-01 21:00:17 -0800

    Update day 10 with modern sensibilities

--- advent2017_day10.py
+++ advent2017_day10.py
@@ -1,59 +1,58 @@
-from itertools import izip_longest
 import operator
+import time
+from functools import reduce
+from itertools import zip_longest
+from typing import Any, Iterable, List
 
-INPUT = """106,16,254,226,55,2,1,166,177,247,93,0,255,228,60,36"""
-
-SALT = [17, 31, 73, 47, 23]
-V2_INPUT = [ord(x) for x in INPUT] + SALT
+from utils import read_data
 
 
-def rotate(to_rotate, amount):
+def rotate(to_rotate: List[int], amount: int) -> List[int]:
     actual_amount = amount % len(to_rotate)
     return to_rotate[actual_amount:] + to_rotate[:actual_amount]
 
 
-def reverse(to_reverse):
+def reverse(to_reverse: List[int]):
     return to_reverse[::-1]
 
 
-def handle_instruction(queue_in, index, skip, length):
+def grouper(iterable: Iterable, n: int, fillvalue: Any = None):
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
+
+
+def handle_instruction(queue_in: List[int], index: int, skip: int, length: int):
     rotated = rotate(queue_in, index)
     rotated[:length] = reverse(rotated[:length])
-    return rotate(rotated, -index), index+length+skip, skip+1
+    return rotate(rotated, -index), index + length + skip, skip + 1
 
 
-def grouper(iterable, n, fillvalue=None):
-    args = [iter(iterable)] * n
-    return izip_longest(*args, fillvalue=fillvalue)
+def get_reduced_hash(knot_hash: List[int]):
+    reduced_hash = []
+    for chunk in grouper(knot_hash, 16):
+        xor_value = reduce(operator.xor, chunk, 0)
+        reduced_hash.append(xor_value)
+    return reduced_hash
 
-knot_hash = list(range(256))
+
+def main():
+    knot_hash = list(range(256))
+    index = skip = 0
+    for instruction in [int(x) for x in read_data().split(",")]:
+        knot_hash, index, skip = handle_instruction(knot_hash, index, skip, instruction)
+    print(f"Part one: {knot_hash[0] * knot_hash[1]}")
+
+    p2_input = [ord(x) for x in read_data()] + [17, 31, 73, 47, 23]
+    knot_hash = list(range(256))
+    index = skip = 0
+    for _ in range(64):
+        for instruction in p2_input:
+            knot_hash, index, skip = handle_instruction(knot_hash, index, skip, instruction)
+    reduced_hash = get_reduced_hash(knot_hash)
+    print(f"Part two: {''.join(format(x, '02x') for x in reduced_hash)}")
 
 
-index = 0
-skip = 0
-for instruction in INPUT.split(","):
-    knot_hash, index, skip = handle_instruction(knot_hash, index, skip, int(instruction))
-
-print("Knot hash v1 is %r" % knot_hash)
-print("First two items of knot hash v1 are %r, multiplied equal %d" % (knot_hash[:2], knot_hash[0] * knot_hash[1]))
-
-knot_hash = list(range(256))
-index = 0
-skip = 0
-
-for i in xrange(64):
-    for instruction in V2_INPUT:
-        knot_hash, index, skip = handle_instruction(knot_hash, index, skip, int(instruction))
-
-print("Knot hash v2 after permutation: %r" % knot_hash)
-
-reduced_hash = []
-for chunk in grouper(knot_hash, 16):
-    # print("Chunk is %r" % list(chunk))
-    xor_value = reduce(operator.xor, chunk, 0)
-    # print("XOR of chunk is %r" % xor_value)
-    reduced_hash.append(xor_value)
-
-print("Reduced v2 hash is %r" % reduced_hash)
-print("Hex string of v2 hash is %s" % ''.join([format(x, '02x') for x in reduced_hash]))
-
+if __name__ == "__main__":
+    start = time.monotonic()
+    main()
+    print(f"Time: {time.monotonic()-start}")
